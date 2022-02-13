5b4d4ea6c017ea9aea757156b1160a58999840c9
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2022-02-13 23:08:11 +0100

    Auxiliary functions added, sockets doesn't work yet

--- hinfosvc.c
+++ hinfosvc.c
@@ -6,17 +6,17 @@
  * ************************************************ */
 
 /*
-1) vytvořte si funkci, která vám vrátí aktuální využití procesoru
-2) zjistete si hostname (/proc/sys/kernel/hostname)
-3) zjistete si cpu name (popen("cat /proc/cpuinfo | grep "model name" | head -n 1 | awk '...)
-4) socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); - vytvoří socket
-5) setsockopt na 1 pro SO_REUSEADDR a SO_REUSEPORT - zajistí že nebude záhadně padat při testování
-6) struct sockaddr_in address;
-   address.sin_family = AF_INET;
-   address.sin_port = htons(tvujPort);
-   address.sin_addr.s_addr = INADDR_ANY;
-7) bind(socket, address) - nastaví socket
-8) listen() - socket začne přijímat spojení
+1) OK vytvořte si funkci, která vám vrátí aktuální využití procesoru
+2) OK zjistete si hostname (/proc/sys/kernel/hostname)
+3) OK zjistete si cpu name (popen("cat /proc/cpuinfo | grep "model name" | head -n 1 | awk '...)
+4) OK socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); - vytvoří socket
+5) OK setsockopt na 1 pro SO_REUSEADDR a SO_REUSEPORT - zajistí že nebude záhadně padat při testování
+6) OK struct sockaddr_in address;
+      address.sin_family = AF_INET;
+      address.sin_port = htons(tvujPort);
+      address.sin_addr.s_addr = INADDR_ANY;
+7) OK bind(socket, address) - nastaví socket
+8) OK listen() - socket začne přijímat spojení
 9) while true do accept() - pak můžete se socketem pracovat jako se souborem
 10) pokud spojení začíná na "GET /hostname" (strncmp) nebo jiný z těch tří endpointů...
 11) tak do socketu zapiš "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n" a tvou odpověď, bez nového řádku na konci
@@ -31,6 +31,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 /** 
  * Function parses arguments and returns the port number.
@@ -105,6 +110,64 @@ int hostname() {
   return 0;
 }
 
+enum cpu_info {USER, NICE, SYSTEM, IDLE, IOWAIT, IRQ, SOFTIRQ, STEAL, GUEST, GUEST_NICE};
+
+/**
+ * Function loads stats from /proc/stat file into array.
+ *
+ * @return 0 if successful, -1 if not
+ */
+int load_stats(long long unsigned a[10]) {
+  FILE *p = popen("cat /proc/stat | head -n 1","r");
+  if (p == NULL) {
+    fprintf(stderr, "/proc/stat did not open successfully.\n");
+    return -1;
+  }
+
+  char info[1024];
+  if (fgets(info, sizeof(info) - 1, p) == NULL) {
+    fprintf(stderr, "Memory allocation failed.\n");
+    pclose(p);
+    return -1;
+  }
+  // save current info
+  sscanf(info, "cpu  %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+    &a[USER], &a[NICE], &a[SYSTEM], &a[IDLE], &a[IOWAIT], &a[IRQ], &a[SOFTIRQ], &a[STEAL], &a[GUEST], &a[GUEST_NICE]);
+  pclose(p);
+
+  return 0;
+}
+
+
+/**
+ * Function computes the CPU load info.
+ * The implementation was inspired by:
+ * https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+ *
+ * @return load percentage
+ */
+int compute_load(long long unsigned f[10], long long unsigned s[10]) {
+  printf("Inside compute_load(), f: %llu, s: %llu\n", f[USER], s[USER]);
+
+  long long int prevIdle = f[IDLE] + f[IOWAIT];
+  long long int idle = s[IDLE] + s[IOWAIT];
+
+  long long int prevNonIdle = f[USER] + f[NICE] + f[SYSTEM] + f[IRQ] + f[SOFTIRQ] + f[STEAL];
+  long long int nonIdle = s[USER] + s[NICE] + s[SYSTEM] + s[IRQ] + s[SOFTIRQ] + s[STEAL];
+
+  long long int prevTotal = prevIdle + prevNonIdle;
+  long long int total = idle + nonIdle;
+
+  long long int totald = total - prevTotal;
+  long long int idled = idle - prevIdle;
+
+  double cpu_percentage = (totald - idled) / (double)totald;
+
+  // round and convert to percentage
+  // TODO not sure with the rounding and * 100
+  return ((int)(cpu_percentage + 0.5)) * 100;
+}
+
 
 /**
  * Function computes the CPU load info
@@ -113,8 +176,16 @@ int hostname() {
  * @return 0 if successful, -1 if not
  */
 int cpu_load() {
+  long long unsigned f[10], s[10];
 
-  return -1;
+  load_stats(f);
+  sleep(1);
+  load_stats(s);
+
+  int load = compute_load(f, s);
+
+  printf("Done cpu_load(), f: %llu, s: %llu, load: %d\n", f[USER], s[USER], load);
+  return 0;
 }
 
 
@@ -133,6 +204,81 @@ int main(int argc, char **argv) {
   hostname();
   
   cpu_name();
+
+  cpu_load();
+
+  // TODO Jindra mel misto 0 IPPROTO_TCP
+  int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  // setsockopt na 1 pro SO_REUSEADDR a SO_REUSEPORT - zajistí že nebude záhadně padat při testování
+  int opt = 1;
+  setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  address.sin_addr.s_addr = INADDR_ANY;
+
+  bind(socketfd, (struct sockaddr*)&address, sizeof(address));
+
+  listen(socketfd, 1);
+
+  /*
+9) while true do accept() - pak můžete se socketem pracovat jako se souborem
+10) pokud spojení začíná na "GET /hostname" (strncmp) nebo jiný z těch tří endpointů...
+11) tak do socketu zapiš "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n" a tvou odpověď, bez nového řádku na konci
+12) Jinak zapiš odpověď se stavem "400 Bad Request"
+13) Pokud tak ještě neučinils, vyčti zbytek dat v socketu ať si třeba prohlížeč nestěžuje
+14) close()  end while
+  */
+
+  char server_message[2000], client_message[2000];
+    
+  // Clean buffers:
+  memset(server_message, '\0', sizeof(server_message));
+  memset(client_message, '\0', sizeof(client_message));
+
+  struct sockaddr client_addr;
+  unsigned int client_size = sizeof(client_addr);
+  int client_sock;
+
+  while (1) {
+    
+    /*
+    client_sock = accept(socketfd, (struct sockaddr*)&client_addr, &client_size);
+
+    //recv(client_sock, client_message, sizeof(client_message), 0);
+    
+    if (client_sock < 0){
+        printf("Can't accept\n");
+        return -1;
+    }
+    //printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    
+    // Receive client's message:
+    if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
+        printf("Couldn't receive\n");
+        return -1;
+    }
+    printf("Msg from client: %s\n", client_message);
+    
+    // Respond to client:
+    strcpy(server_message, "This is the server's message.");
+    
+    if (send(client_sock, server_message, strlen(server_message), 0) < 0){
+        printf("Can't send\n");
+        return -1;
+    }
+
+    //send(client_sock, server_message, strlen(server_message), 0);
+
+    printf("Client: %s", client_message);
+    //printf("Server: %s", server_message);
+    close(client_sock);
+    */
+
+  }
+
+  close(socketfd);
 
   return 0;
 }
