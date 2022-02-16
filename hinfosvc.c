1012e1a4a73cc7f22f240688b74c01f49248f4a8
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2022-02-16 15:15:43 +0100

    communication works, signal handler added

--- hinfosvc.c
+++ hinfosvc.c
@@ -17,6 +17,10 @@
 #include <string.h>
 #include <signal.h>
 
+// CPU stats
+enum cpu_info {USER, NICE, SYSTEM, IDLE, IOWAIT, IRQ, SOFTIRQ, STEAL, GUEST, GUEST_NICE};
+// server socket descriptor
+int socketfd;
 
 /** 
  * Function parses arguments and returns the port number.
@@ -45,9 +49,9 @@ int get_port_num(int argc, char **argv) {
 
 /**
  * Functions loads the hostname info from /proc/sys/kernel/hostname 
- * file and prints it to the stdout.
+ * file and saves it to the message buffer.
  *
- * @return 0 if successful, -1 if the file couldn't be open
+ * @return pointer to the message buffer, NULL in the case of allocation error
  */
 char *hostname(char *message, unsigned *capacity) {
   // open file with the hostname information
@@ -57,13 +61,14 @@ char *hostname(char *message, unsigned *capacity) {
     return NULL;
   }
 
-  // read and print the hostname to the response
+  // read and save the hostname to the response
   int c;
   sprintf(message, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
   unsigned i = strlen("HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n");
   
   while ((c = fgetc(fp)) != EOF) {
     if (*capacity <= i) {
+      // reallocate the buffer if its capacity is full
       message = realloc(message, *capacity * 2);
       *capacity *= 2;
     }
@@ -82,9 +87,9 @@ char *hostname(char *message, unsigned *capacity) {
 
 /**
  * Function gets the CPU name info from /proc/cpuinfo
- * file and prints it to the stdout.
+ * file and saves it to the message buffer.
  *
- * @return 0 if successful, -1 if not
+ * @return pointer to the message buffer, NULL in the case of allocation error
  */
 char *cpu_name(char *message, unsigned *capacity) {
   // open a process
@@ -116,9 +121,6 @@ char *cpu_name(char *message, unsigned *capacity) {
   return message;
 }
 
-
-// CPU stats
-enum cpu_info {USER, NICE, SYSTEM, IDLE, IOWAIT, IRQ, SOFTIRQ, STEAL, GUEST, GUEST_NICE};
 
 /**
  * Function loads stats from /proc/stat file into array.
@@ -174,84 +176,126 @@ double compute_load(long long unsigned f[10], long long unsigned s[10]) {
 
 
 /**
- * Function computes the CPU load info
- * and prints it to the stdout.
+ * Function computes the CPU load info and saves it to the message buffer.
  *
- * @return 0 if successful, -1 if not
+ * @return pointer to the message buffer, NULL in the case of allocation error
  */
 char *cpu_load(char *message) {
   long long unsigned f[10], s[10];
 
-  load_stats(f);
+  if (load_stats(f) == -1) {
+    return NULL;
+  }
+
   sleep(1);
-  load_stats(s);
+
+  if (load_stats(s) == -1) {
+    return NULL;
+  }
 
   double load = compute_load(f, s);
 
-  // read and print the load (initial capacity is always bigger)
+  // read and print the load (initial capacity is always bigger - no need to realloc)
   sprintf(message, "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n%lg%c", load, '%');
   return message;
 }
 
 
+/**
+ * Function writes "bad request" response to the message buffer.
+ *
+ * @return pointer to the message buffer
+ */
 char *bad_req(char *message) {
   sprintf(message, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain;\r\n\r\nBad Request");
   return message;
 }
 
 
-int main(int argc, char **argv) {  
+/**
+ * Function closes server socket and exits the program.
+ * It is called when SIGINT signal is received.
+ */
+void release_resources(int sig_num){
+  fprintf(stderr, "[signal %d] -> server socket was closed\n", sig_num);
+  close(socketfd);
+  exit(0); 
+}
+
+
+/**
+ * Main function.
+ */
+int main (int argc, char **argv) {  
   // get the port number
   int port = get_port_num(argc, argv);
   if (port == -1) {
     return -1;
   }
 
-  int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  // server socket
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
   if (socketfd == -1) {
 		fprintf(stderr, "Could not create socket");
+    return -1;
 	}
-
   int opt = 1;
   setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
+  
   struct sockaddr_in server_address;
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(port);
   server_address.sin_addr.s_addr = INADDR_ANY;
-
   if (bind(socketfd, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
     fprintf(stderr, "Binding error.\n");
+    return -1;
   }
-
   if (listen(socketfd, 1) == -1) {
     fprintf(stderr, "Listening error.\n");
+    return -1;
   }
 
+  // client socket, client message
   struct sockaddr client_addr;
   unsigned int client_size = sizeof(client_addr);
   int client_sockfd;
-  char client_message[2000];
+  char client_message[1000];
   memset(client_message, '\0', sizeof(client_message));
 
+  // server response
   unsigned response_capacity = strlen("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain;\r\n\r\nBad Request");
   char *server_response = calloc(response_capacity, 1);
   if (server_response == NULL) {
     fprintf(stderr, "Couldn't allocate the memory.\n");
+    return -1;
   }
   memset(server_response, '\0', response_capacity);
 
+  // SIGINT signal handler
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = release_resources;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  
+  /* server is running and sending responses; in the case of any error with the client socket 
+     the client socket is closed and the server is waiting for receiving a new client socket */
   while (1) {
+    // closing all resources if the SIGINT signal was received
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    // receive the client socket
     client_sockfd = accept(socketfd, (struct sockaddr*)&client_addr, &client_size);
     if (client_sockfd == -1) {
       fprintf(stderr, "Client socked not received successfully.\n");
+      continue;
     }
-
     if (read(client_sockfd, &client_message, sizeof(client_message)) == -1) {
-      fprintf(stderr, "Reading error.\n");
+      fprintf(stderr, "Reading error, client socket closed.\n");
+      close(client_sockfd);
+      continue;
     }
     
-    // query check
+    // query check, send the response
     if (strncmp(client_message, "GET /hostname ", 14) == 0) {
       server_response = hostname(server_response, &response_capacity);
     } else if (strncmp(client_message, "GET /cpu-name ", 14) == 0) {
@@ -261,18 +305,23 @@ int main(int argc, char **argv) {
     } else {
       server_response = bad_req(server_response);
     }
-
+    if (server_response == NULL) {
+      close(client_sockfd);
+      continue;
+    }
     if (send(client_sockfd, server_response, strlen(server_response), 0) == -1){
       fprintf(stderr, "Can't send a response.\n");
+      close(client_sockfd);
+      continue;
     }
 
+    // close the client socket, clear messages
     memset(client_message, '\0', sizeof(client_message));
     memset(server_response, '\0', response_capacity);
-    
     close(client_sockfd);
   }
 
+  // close server socket 
   close(socketfd);
-
   return 0;
 }
